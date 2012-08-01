cab80b9d50397060020b6b1b5d32ff87c8cd0993
Florian Jungwirth <jungwirth.florian@gmail.com>
2012-08-01 11:00:28 +0200

    scorn added

---
+++ lib/lib.php
@@ -0,0 +1,39 @@
+<?php
+function filenameReplaceBadChars( $filename, $folder=false ) {
+
+	$patterns = array(
+			"/\\s/",  # Leerzeichen
+			"/\\&/",  # Kaufmaennisches UND
+			"/\\+/",  # Plus-Zeichen
+			"/\\</",  # < Zeichen
+			"/\\>/",  # > Zeichen
+			"/\\?/",  # ? Zeichen
+			"/\"/",   # " Zeichen
+			"/\\:/",  # : Zeichen
+			"/\\|/",  # | Zeichen
+			"/\\\\/", # \ Zeichen
+			"/\\*/"   # * Zeichen
+	);
+	if(!$folder)
+		$patterns[] = "/\\//";  # / Zeichen
+
+	$replacements = array(
+			" ",
+			" ",
+			" ",
+			" ",
+			" ",
+			" ",
+			" ",
+			" ",
+			" ",
+			" ",
+			" "
+	);
+	if(!$folder)
+		$replacements[] = "";
+
+	return preg_replace( $patterns, $replacements, $filename );
+	 
+}
+?>
